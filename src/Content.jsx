985c21eb81e8387719668132619d21975b066b8c
Viktor Bogutskii <info@siteograf.com>
2020-02-09 21:50:23 -0800

    props

--- src/Content.jsx
+++ src/Content.jsx
@@ -5,21 +5,20 @@ function Content() {
   let name = 'Initial';
 
   const clickButtonHandler = (value) => {
-    console.log('Clicked! ' + name)
+    console.log('Clicked! ' + name);
   };
 
   const inputHandler = (e) => {
     name = e.target.value;
-    console.log(e.target.value)
+    console.log(e.target.value);
   };
 
   return (
     <main>
       <div className="container">
         <h1>First app</h1>
-        <h1>First app</h1>
         {name}
-        <input type="text" onChange={inputHandler}/>
+        <input type="text" onChange={inputHandler} />
 
         <button onClick={() => clickButtonHandler(1)}>Add one</button>
         <button onClick={() => clickButtonHandler(2)}>Add two</button>
